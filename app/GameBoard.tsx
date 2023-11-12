dd87da74dc573c3d4d48c7ae1ff8e72a9f07d0ff
Xero1000 <alex.heil@comcast.net>
2023-11-12 08:49:36 -0500

    Create moveDown function

--- app/GameBoard.tsx
+++ app/GameBoard.tsx
@@ -22,17 +22,17 @@ const GameBoard = () => {
   const moveLeft = () => {
     setBoardData((currentBoard) => {
       let newBoard = currentBoard.map((row) => [...row]);
-      for (let i = 0; i < 4; i++) {
-        for (let j = 1; j < 4; j++) {
-          if (newBoard[i][j] !== 0) {
-            for (let k = j - 1; k >= 0; k--) {
-              if (newBoard[i][k] === 0) {
-                newBoard[i][k] = newBoard[i][k + 1];
-                newBoard[i][k + 1] = 0;
+      for (let r = 0; r < 4; r++) {
+        for (let c = 1; c < 4; c++) {
+          if (newBoard[r][c] !== 0) {
+            for (let k = c - 1; k >= 0; k--) {
+              if (newBoard[r][k] === 0) {
+                newBoard[r][k] = newBoard[r][k + 1];
+                newBoard[r][k + 1] = 0;
               }
-              else if (newBoard[i][k] === newBoard[i][k + 1]) {
-                newBoard[i][k] *= 2
-                newBoard[i][k + 1] = 0
+              else if (newBoard[r][k] === newBoard[r][k + 1]) {
+                newBoard[r][k] *= 2
+                newBoard[r][k + 1] = 0
               }
             }
           }
@@ -45,17 +45,17 @@ const GameBoard = () => {
   const moveRight = () => {
     setBoardData((currentBoard) => {
       let newBoard = currentBoard.map((row) => [...row]);
-      for (let i = 0; i < 4; i++) {
-        for (let j = 2; j >= 0; j--) {
-          if (newBoard[i][j] !== 0) {
-            for (let k = j + 1; k < 4; k++) {
-              if (newBoard[i][k] === 0) {
-                newBoard[i][k] = newBoard[i][k - 1];
-                newBoard[i][k - 1] = 0;
+      for (let r = 0; r < 4; r++) {
+        for (let c = 2; c >= 0; c--) {
+          if (newBoard[r][c] !== 0) {
+            for (let k = c + 1; k < 4; k++) {
+              if (newBoard[r][k] === 0) {
+                newBoard[r][k] = newBoard[r][k - 1];
+                newBoard[r][k - 1] = 0;
               }
-              else if (newBoard[i][k] === newBoard[i][k - 1]) {
-                newBoard[i][k] *= 2
-                newBoard[i][k - 1] = 0
+              else if (newBoard[r][k] === newBoard[r][k - 1]) {
+                newBoard[r][k] *= 2
+                newBoard[r][k - 1] = 0
               }
             }
           }
@@ -64,6 +64,31 @@ const GameBoard = () => {
       return newBoard
     })
   };
+
+  const moveDown = () => {
+    setBoardData((currentBoard) => {
+      let newBoard = currentBoard.map((row) => [...row]);
+      for (let c = 0; c < 4; c++) {
+        for (let r = 2; r >= 0; r--) {
+          if (newBoard[r][c] !== 0) {
+            for (let k = r + 1; k < 4; k++) {
+              if (newBoard[k][c] === 0) {
+                newBoard[k][c] = newBoard[k - 1][c];
+                newBoard[k - 1][c] = 0;
+              }
+              else if (newBoard[k][c] === newBoard[k - 1][c]) {
+                newBoard[k][c] *= 2
+                newBoard[k - 1][c] = 0
+              }
+            }
+          }
+        }
+      }
+      return newBoard
+    })
+  };
+
+  
 
   useEffect(() => {
     initializeBoard();
@@ -79,7 +104,7 @@ const GameBoard = () => {
           moveLeft();
           break;
         case "s":
-          console.log("s");
+          moveDown()
           break;
         case "d":
           moveRight()
