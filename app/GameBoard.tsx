b260e0df4644032eded627f2ff4bf2791128437b
Xero1000 <alex.heil@comcast.net>
2023-11-14 08:59:56 -0500

    Fix merge bug for moveLeft and moveRight functions

--- app/GameBoard.tsx
+++ app/GameBoard.tsx
@@ -7,17 +7,17 @@ import generateTile from "./utilities/generateTile";
 
 const GameBoard = () => {
   const [boardData, setBoardData] = useState([
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [4, 4, 2, 2],
+    [2, 2, 2, 2],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ]);
 
   // previous state of the board
-  const [prevBoardData, setPrevBoardData] = useState(boardData)
+  const [prevBoardData, setPrevBoardData] = useState(boardData);
 
   // variable to track if a move was made
-  const [moveMade, setMoveMade] = useState(false)
+  const [moveMade, setMoveMade] = useState(false);
 
   // Runs generateTile twice upon startup
   const initializeBoard = () => {
@@ -30,67 +30,85 @@ const GameBoard = () => {
   const checkBoardChange = () => {
     for (let r = 0; r < 4; r++) {
       for (let c = 0; c < 4; c++) {
-        if (boardData[r][c] !== prevBoardData[r][c])
-          return true
+        if (boardData[r][c] !== prevBoardData[r][c]) return true;
       }
     }
-    return false
-  }
+    return false;
+  };
 
   const moveLeft = () => {
     // use functional update to ensure the most current board
     // state is used
     setBoardData((currentBoard) => {
-      setPrevBoardData(currentBoard) // save board state prior to change
-
+      setPrevBoardData(currentBoard); // save board state prior to change
       let newBoard = currentBoard.map((row) => [...row]);
       for (let r = 0; r < 4; r++) {
+        let tilesWithMerge: number[] = []
         for (let c = 1; c < 4; c++) {
           if (newBoard[r][c] !== 0) {
-            for (let k = c - 1; k >= 0; k--) {
-              if (newBoard[r][k] === 0) {
+            if (newBoard[r][c - 1] === 0) {
+              let k = c - 1;
+              while (newBoard[r][k] === 0) {
                 newBoard[r][k] = newBoard[r][k + 1];
                 newBoard[r][k + 1] = 0;
-              } else if (newBoard[r][k] === newBoard[r][k + 1]) {
-                newBoard[r][k] *= 2;
-                newBoard[r][k + 1] = 0;
+                k--;
               }
+              if (newBoard[r][k] === newBoard[r][k + 1] && !tilesWithMerge.includes(k)) {
+                newBoard[r][k] = newBoard[r][k + 1] * 2;
+                newBoard[r][k + 1] = 0;
+                tilesWithMerge.push(k)
+              }
+            } else if (newBoard[r][c - 1] === newBoard[r][c] && !tilesWithMerge.includes(c - 1)) {
+              newBoard[r][c - 1] *= 2;
+              newBoard[r][c] = 0;
+              tilesWithMerge.push(c - 1)
             }
           }
         }
       }
-      setMoveMade(true)
+      setMoveMade(true);
       return newBoard;
     });
   };
 
   const moveRight = () => {
+    // use functional update to ensure the most current board
+    // state is used
     setBoardData((currentBoard) => {
-      setPrevBoardData(currentBoard)
+      setPrevBoardData(currentBoard); // save board state prior to change
       let newBoard = currentBoard.map((row) => [...row]);
       for (let r = 0; r < 4; r++) {
+        let tilesWithMerge: number[] = []
         for (let c = 2; c >= 0; c--) {
           if (newBoard[r][c] !== 0) {
-            for (let k = c + 1; k < 4; k++) {
-              if (newBoard[r][k] === 0) {
+            if (newBoard[r][c + 1] === 0) {
+              let k = c + 1;
+              while (newBoard[r][k] === 0) {
                 newBoard[r][k] = newBoard[r][k - 1];
                 newBoard[r][k - 1] = 0;
-              } else if (newBoard[r][k] === newBoard[r][k - 1]) {
-                newBoard[r][k] *= 2;
-                newBoard[r][k - 1] = 0;
+                k++;
               }
+              if (newBoard[r][k] === newBoard[r][k - 1] && !tilesWithMerge.includes(k)) {
+                newBoard[r][k] = newBoard[r][k - 1] * 2;
+                newBoard[r][k - 1] = 0;
+                tilesWithMerge.push(k)
+              }
+            } else if (newBoard[r][c + 1] === newBoard[r][c] && !tilesWithMerge.includes(c + 1)) {
+              newBoard[r][c + 1] *= 2;
+              newBoard[r][c] = 0;
+              tilesWithMerge.push(c + 1)
             }
           }
         }
       }
-      setMoveMade(true)
+      setMoveMade(true);
       return newBoard;
     });
   };
 
   const moveDown = () => {
     setBoardData((currentBoard) => {
-      setPrevBoardData(currentBoard)
+      setPrevBoardData(currentBoard);
       let newBoard = currentBoard.map((row) => [...row]);
       for (let c = 0; c < 4; c++) {
         for (let r = 2; r >= 0; r--) {
@@ -107,14 +125,14 @@ const GameBoard = () => {
           }
         }
       }
-      setMoveMade(true)
+      setMoveMade(true);
       return newBoard;
     });
   };
 
   const moveUp = () => {
     setBoardData((currentBoard) => {
-      setPrevBoardData(currentBoard)
+      setPrevBoardData(currentBoard);
       let newBoard = currentBoard.map((row) => [...row]);
       for (let c = 0; c < 4; c++) {
         for (let r = 1; r < 4; r++) {
@@ -131,19 +149,18 @@ const GameBoard = () => {
           }
         }
       }
-      setMoveMade(true)
+      setMoveMade(true);
       return newBoard;
     });
   };
 
   // board is initialized with two tiles upon startup
-  useEffect(() => {
-    initializeBoard();
-  }, []);
+  // useEffect(() => {
+  //   initializeBoard();
+  // }, []);
 
-  // 
+  //
   useEffect(() => {
-
     const handleKeyPress = (e: KeyboardEvent) => {
       switch (e.key) {
         case "w": // pressing 'w' will move tiles up
@@ -173,17 +190,17 @@ const GameBoard = () => {
   }, []);
 
   // When a move is made, if the board changed at all from the
-  // previous board state, a new tile will be generated 
+  // previous board state, a new tile will be generated
   // in an empty random tile.
   useEffect(() => {
     if (moveMade) {
       if (checkBoardChange()) {
-        const newBoard = generateTile(boardData)
-        setBoardData(newBoard)
+        const newBoard = generateTile(boardData);
+        setBoardData(newBoard);
       }
-      setMoveMade(false)
+      setMoveMade(false);
     }
-  }, [moveMade])
+  }, [moveMade]);
 
   return (
     <Grid
