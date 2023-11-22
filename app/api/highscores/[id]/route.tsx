8516f19fbc21a887ec3248d96ad067f9c889ca68
Xero1000 <alex.heil@comcast.net>
2023-11-22 08:45:12 -0500

    Install zod and create files for route handlers

---
+++ app/api/highscores/[id]/route.tsx
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+    params: { id: number }
+}
+
+export function GET(request: NextRequest, { params }: Props) {
+    if (params.id > 10) 
+        return NextResponse.json({ error: "highscore not found" }, { status: 404 })
+
+    return NextResponse.json({ id: 1, name: "Mosh" })
+} 
+
+export function DELETE(request: NextRequest, { params }: Props) {
+    if (params.id > 10)
+        return NextResponse.json({ error: "User not found" }, { status: 404 })
+
+    return NextResponse.json({})
+}
---
+++ app/api/highscores/route.tsx
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+
+export async function GET(request: NextRequest) {
+    const body = await request.json()
+    return NextResponse.json("hello")
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const validation = schema.safeParse(body)
+
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 })
+    return NextResponse.json({ name: body.name, score: body.score }, { status: 201 })
+}
+
