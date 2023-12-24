3657765476ef700b834f799cf1699a5b6538b4f4
Xero1000 <alex.heil@comcast.net>
2023-12-24 13:08:39 -0500

    Refactor: Implement react query with load game modal

--- app/LoadGameModal.tsx
+++ app/LoadGameModal.tsx
@@ -1,7 +1,10 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import isModalOpenContext from "./state-management/contexts/isModalOpenContext";
 import axios from "axios";
 import gameDataContext from "./state-management/contexts/gameDataContext";
+import { useQuery } from "@tanstack/react-query";
+import { SavedGame } from "@prisma/client";
+import Spinner from "./components/Spinner";
 
 interface Props {
   isLoadGameModalOpen: boolean;
@@ -9,29 +12,74 @@ interface Props {
 }
 
 const LoadGameModal = ({ isLoadGameModalOpen, onClose }: Props) => {
-  const { setBoardData, setScore, setGameOver, setWin, setLose } = useContext(gameDataContext);
+  const { setBoardData, setScore, setGameOver, setWin, setLose } =
+    useContext(gameDataContext);
   const { setIsModalOpen } = useContext(isModalOpenContext);
 
+  const {
+    data: savedGame,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery<SavedGame>({
+    queryKey: ["savedGame"],
+    queryFn: () =>
+      axios.get<SavedGame>("/api/savedGame").then((res) => res.data),
+  });
+
+  const [noSavedGame, setNoSavedGame] = useState(false);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
-    if (isLoadGameModalOpen) setIsModalOpen(true);
-    else setIsModalOpen(false);
+    if (isLoadGameModalOpen) {
+      setIsModalOpen(true);
+      refetch();
+    } else {
+      setIsModalOpen(false);
+    }
   }, [isLoadGameModalOpen]);
 
-  const loadGame = async () => {
-    try {
-      const savedGame = (await axios.get("/api/savedGame")).data;
-      const boardData = JSON.parse(savedGame.boardData);
-      const score = savedGame.score;
+  // Cleanup function to clear the timeout
+  // This is to prevent potential side effects if the component unmounts
+  // before the timeout completes.
+  useEffect(() => {
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [timeoutId]);
 
-      setBoardData(boardData)
-      setScore(score)
-      setGameOver(false)
-      setWin(false)
-      setLose(false)
-      onClose()
-    } catch (error) {
-      console.log("Error loading game: ", error);
+  const loadGame = async () => {
+    if (error) {
+      return;
     }
+
+    // if there is no currently saved game for the user
+    if (!savedGame) {
+      setNoSavedGame(true);
+      return;
+    }
+    setNoSavedGame(false);
+
+    const boardData = JSON.parse(savedGame.boardData);
+    const score = savedGame.score;
+
+    setBoardData(boardData);
+    setScore(score);
+    setGameOver(false);
+    setWin(false);
+    setLose(false);
+    onClose();
+  };
+
+  const closeModal = () => {
+    // Keep NoSavedGame message open as modal closes
+    const id = setTimeout(() => {
+      setNoSavedGame(false);
+    }, 1000);
+    setTimeoutId(id);
+    onClose(); // close the modal
   };
 
   return (
@@ -43,20 +91,37 @@ const LoadGameModal = ({ isLoadGameModalOpen, onClose }: Props) => {
         <p className="py-4 text-center">
           This will overrite your current game progress
         </p>
+        {error ? (
+          <p className="text-center text-red-600">
+            Error. Cannot load game data
+          </p>
+        ) : isLoading ? (
+          <>
+            <p className="text-center">Fetching data. Please wait...</p>
+            <div className="text-center my-5">
+              <Spinner />
+            </div>
+          </>
+        ) : noSavedGame ? (
+          <p className="text-center text-red-600">No saved data found</p>
+        ) : (
+          ""
+        )}
         <div className="flex justify-center gap-4 py-3">
           <button
             onClick={loadGame}
-            className="btn"
+            className={"btn"}
+            disabled={isLoading || (error ? true : false)}
           >
             Load Game
           </button>
-          <button onClick={onClose} className="btn">
+          <button onClick={closeModal} className="btn">
             Cancel
           </button>
         </div>
       </div>
       <form method="dialog" className="modal-backdrop">
-        <button onClick={onClose}>Close</button>
+        <button onClick={closeModal}>Close</button>
       </form>
     </dialog>
   );
